9ca5c0def45fa2d879c9cdbe2ed92a422e9e48c7
maggie w <margaret.wooten@nasa.gov>
2018-01-29 14:34:41 -0500

    Added query_db.py script and its dependencies

---
+++ transfer_batch.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+batch=$1 # only command line argument is batch
+
+logfile="/att/nobackup/mwooten3/Paul_TTE/queryLogs/batch${batch}_ADAPT_query_log.txt"
+
+printf "\nCopying archive to DISCOVER with rsync. Output can be found in:\n$logfile\n\n"
+
+exec >> $logfile 2>&1 # redirect standard out and error to log file
+
+SECONDS=0
+
+
+printf "\n-------------------------------------------"
+printf "\nCopying archive to DISCOVER with rsync:\n\n"
+printf "START: "
+#start_date=date
+date
+#printf "\n"
+cmd="nohup rsync -avxH --progress /att/nobackup/mwooten3/Paul_TTE/ASP/batch$batch-archive.tar.gz discover.nccs.nasa.gov:/discover/nobackup/projects/boreal_nga/ASP"
+printf " $cmd\n\n"
+eval $cmd
+printf "\n\nEND: "
+#end_date=date
+#printf "$end_date"
+date
+duration=$SECONDS
+printf "\nElapsed time to move batch $batch archive = $(($duration/60)) minutes\n\n"
