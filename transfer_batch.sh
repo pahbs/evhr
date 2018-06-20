2320c007a6f9cf60ad8127428ce6daa208af214b
maggie w <margaret.wooten@nasa.gov>
2018-06-20 19:06:20 -0400

    minor edits to transfer script

--- transfer_batch.sh
+++ transfer_batch.sh
@@ -2,17 +2,16 @@
 
 #batch=$1 # only command line argument is batch
 # Provide second directory argument if not in default dir
-if [ $# -eq 1 ]
-  then
+if [ $# -eq 1 ] ; then
     batch=$1
     adaptDir="/att/nobackup/mwooten3/AIST/TTE/ASP"
 fi
-if [ $# -eq 2 ]
-  then
+if [ $# -eq 2 ] ; then
     batch=$1
     adaptDir=$2
 fi
 
+tarzipBatch=false # set to true if batch was tar/zipped
 
 logfile="/att/nobackup/mwooten3/AIST/TTE/queryLogs/batch${batch}_ADAPT_query_log.txt"
 
@@ -29,8 +28,11 @@ printf "START: "
 #start_date=date
 date
 #printf "\n"
-#cmd="nohup rsync -avxH --progress $adaptDir/batch$batch-archive.tar.gz discover.nccs.nasa.gov:/discover/nobackup/projects/boreal_nga/ASP" # if using tar.gz
-cmd="nohup rsync -avxH --progress $adaptDir/batch$batch  discover.nccs.nasa.gov:/discover/nobackup/projects/boreal_nga/ASP"
+if [ "$tarzipBatch" = true ] ; then
+    cmd="nohup rsync -avxH --progress $adaptDir/batch$batch-archive.tar.gz discover.nccs.nasa.gov:/discover/nobackup/projects/boreal_nga/ASP" # if using tar.gz
+else
+    cmd="nohup rsync -avxH --progress $adaptDir/batch$batch  discover.nccs.nasa.gov:/discover/nobackup/projects/boreal_nga/ASP"
+fi
 printf " $cmd\n\n"
 eval $cmd
 printf "\n\nEND: "
